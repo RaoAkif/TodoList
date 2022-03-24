e7e4d9e09cb13a2e561c876ad1b93e4826b9cf9c
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-24 17:42:19 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './main.css';
-import { items } from './modules/data.js';
+import items from './modules/data.js';
 
-const listItem = document.querySelector('#task-list');
+const listItem = document.querySelector("#task-list");
 
 window.onload = () => {
   items.forEach((item) => {
     listItem.innerHTML += `
-        <li class='item' id='item-${item.index}'>
+      <li class='item' id='item-${item.index}'>
         <div id='item-check'>
             <input type='checkbox' id='item-check' name='item' value='item'>
             <div class='item-spec'>
@@ -16,10 +16,10 @@ window.onload = () => {
             </div>
         </div>
         <div class='item-completed'>
-        <i class='fa-solid fa-ellipsis-vertical'></i>
-        <i class='fa-solid fa-trash-can'></i>
+          <i class='fa-solid fa-ellipsis-vertical'></i>
+          <i class='fa-solid fa-trash-can'></i>
         </div>
-        </li>
-        `;
+      </li>
+    `;
   });
 };
