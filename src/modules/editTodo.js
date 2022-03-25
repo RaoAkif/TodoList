6ec88513754815943de481f1f119e3e5dc9540c7
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-26 01:19:30 +0500

    CRUD Operations
    - Modularization
    - CRUD Operation
    - Add + Remove (Single Entry)

---
+++ src/modules/editTodo.js
@@ -0,0 +1,33 @@
+import { createList } from "./displayList.js";
+import remove from "../assets/images/delete.svg";
+
+const trash = new Image();
+trash.src = remove;
+export default function EditTodo(toDo) {
+  const editButtons = document.querySelectorAll(".dots");
+  for (let i = 0; i < editButtons.length; i += 1) {
+    editButtons[i].addEventListener("click", () => {
+      editButtons[i].parentElement.classList.add("color");
+      editButtons[i].parentElement.innerHTML = `
+        <input class="checkbox" type="checkbox">
+        <input class="edit" type="text" value="${toDo.list[i].description}">
+        <img class="dots" src=${remove}>
+      `;
+      const input = document.querySelector(".edit");
+      input.addEventListener("keypress", (e) => {
+        if (e.key === "Enter" && input.value) {
+          toDo.list[i].description = input.value;
+          toDo.store();
+          createList(toDo);
+        }
+      });
+      const trashCan = document.querySelectorAll(".dots");
+      trashCan[i].addEventListener("click", () => {
+        toDo.remove(i);
+        toDo.fixIndex();
+        toDo.store();
+        window.location.reload();
+      });
+    });
+  }
+}
